cddcc01a9d06301e62222f753eefa4631717359d
datasmurfen <oddvar.tengesdal@hotmail.com>
2014-01-29 10:44:20 +0100

    oppg 2 i lab2.py

--- lab/lab2/lab2.py
+++ lab/lab2/lab2.py
@@ -33,4 +33,13 @@ print """
     (./
      '` 
 
-""";
+"""
+
+# 
+#  Oppgave 2
+#    'return 2' - 2 skal erstattes med en korrekt returverdi, 2 er kun en stedsholder
+#    bitAnd - x&y
+#    Eksempel: bitAnd(6, 5) = 4
+#
+def bitAnd(x, y):
+  return 2
